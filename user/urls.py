3d0831e38de6c3057b60e7c9f96ecec8d850cf87
3661sy <flsk3661@gmail.com>
2024-09-09 22:53:50 +0900

    feat: add token refresh api

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from user.views import UserRegisterView, UserLoginView
 
 urlpatterns = [
     path('/register', UserRegisterView.as_view(), name='user-register'),
+    path('/refresh', TokenRefreshView.as_view(), name='token-refresh'),
     path('/login', UserLoginView.as_view(), name='user-login')
 ]
